592dd4fe6a5e1ac0bce4def873882f48d11bf42e
zachary smith <zachary smith>
2017-03-29 13:04:08 -0500

    z

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,25 +1,40 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDYxbNaRv0I_mVKKshJqtZw40_PXanVXlQ",
-    authDomain: "employeetime-969c9.firebaseapp.com",
-    databaseURL: "https://employeetime-969c9.firebaseio.com",
-    storageBucket: "employeetime-969c9.appspot.com",
-    messagingSenderId: "922747148509"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function () {  
 
-  // Create a variable to reference the database.
-var database = firebase.database();
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyDYxbNaRv0I_mVKKshJqtZw40_PXanVXlQ",
+	    authDomain: "employeetime-969c9.firebaseapp.com",
+	    databaseURL: "https://employeetime-969c9.firebaseio.com",
+	    storageBucket: "employeetime-969c9.appspot.com",
+	    messagingSenderId: "922747148509"
+	  };
+	 firebase.initializeApp(config);
 
-var name startDate monthRate;
+	  // Create a variable to reference the database.
+	var database = firebase.database();
 
-// When ever employee hits submit button
-$("#submit").on("click", function(){
-	// get employee input values
-	name = $("#").val().trim();
-	startDate = $("#").val().trim();
-	monthRate =  parseInt($("#").val().trim());
+	var name startDate monthRate;
 
-	// console.log user values 
-	
+	// When ever employee hits submit button
+	$("#search-button").on("click", function(){
+		// get employee input values
+		name = $("#").val().trim();
+		startDate = $("#").val().trim();
+		monthRate =  parseInt($("#").val().trim());
+
+		
+
+		var tRow = $("<tr>");
+		tRow.append("<td>"input"</td>");
+		tRow.append("<td>"input"</td>");
+		tRow.append("<td>"input"</td>");
+		tRow.append("<td>"input"</td>");
+		tRow.append("<td>"input"</td>");
+		tRow.append("<td>"input"</td>");
+		$("#employee-table").append(tRow);
+
+		console.log(tRow);
+		
+		
+	})
 })
