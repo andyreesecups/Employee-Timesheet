38f9085e81b560ceeec6a8dc2effae4f011d4c6e
Jillian Flook <jkenw10@gmail.com>
2017-03-30 10:21:56 -0500

    converted months worked and total billed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,12 +19,14 @@ $(document).ready(function () {
 	var startDate; 
 	var monthRate; 
 	var role;
-	var totalBilled;
+	//var totalBilled;
 	var monthsWorked;
+	var newMonth;
 
 // When ever employee hits submit button
 	$("#search-button").on("click", function(event){
 	
+	event.preventDefault();
 	// get employee input values
 	name = $("#name").val().trim();
 	role = $("#role").val().trim();
@@ -49,26 +51,52 @@ $(document).ready(function () {
 }); //end search button
 
 database.ref().on("child_added", function(snapshot){
-	$("#name").html(snapshot.val().name);
-	$("#role").html(snapshot.val().role);
-	$("#start-date").html(snapshot.val().startDate);
-	$("#monthly-rate").html(snapshot.val().monthRate);
+	console.log(snapshot.val());
+	// $("#name").html(snapshot.val().name);
+	// $("#role").html(snapshot.val().role);
+	// $("#start-date").html(snapshot.val().startDate);
+	// $("#monthly-rate").html(snapshot.val().monthRate);
 
+	calcMonths(snapshot.val().startDate);
+
+	var totalBilled = newMonth * snapshot.val().monthRate;
+	console.log(totalBilled);
+
+	//calcBill(snapshot.val().monthRate, snapshot.val().newMonth);
 	 //adding user input to the table
-    var tRow = $("<tr>");
-		tRow.append("<td>"+ name+ "</td>");
-		tRow.append("<td>"+ role + "</td>");
-		tRow.append("<td>"+ startDate + "</td>");
-		tRow.append("<td>"+ monthsWorked +"</td>");
-		tRow.append("<td>"+ monthRate + "</td>");
+	  var tRow = $("<tr>");
+		tRow.append("<td>"+ snapshot.val().name + "</td>");
+		tRow.append("<td>"+ snapshot.val().role + "</td>");
+		tRow.append("<td>"+ snapshot.val().startDate + "</td>");
+		tRow.append("<td>"+ newMonth +"</td>");
+		tRow.append("<td>"+ snapshot.val().monthRate + "</td>");
 		tRow.append("<td>"+ totalBilled +"</td>");
 		$("#employee-table").append(tRow);
 		console.log(name);
 
+
+		
 	},
 	function(errorObject){
 		console.log("errors handled:" + errorObject.code);
 });
+
+function calcMonths(sd){
+
+	//take sd and format it in unix time THENNNNN do diff opp  THHEENNNN to months
+	 var unixStart = moment(sd).format('X');
+
+	 console.log(unixStart);
+
+
+	var convertStart = moment(new Date(sd));
+	console.log(convertStart + "CONVERT START");
+	console.log(moment().diff(moment(convertStart), "months"));
+    newMonth = moment().diff(moment(convertStart), "months");
+	// monthsWorked = moment(startDate , "month").fromNow()
+};
+
+
 
 
 }); //end of document ready
